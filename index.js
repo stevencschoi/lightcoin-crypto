7a48dd24ba8c4b398bbd7702b04ffa59e8f8d92c
Steven Choi <stevencschoi87@gmail.com>
2020-02-14 22:02:29 +0000

    refactoring with classes

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
+}
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+}
+
+class Deposit extends Transaction {
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  commit() {
+    this.account.balance -= this.amount;
   }
 
 }
 
-class Deposit {
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance += this.amount;
-  }
-}
-
-
+const myAccount = new Account("snow-patrol");
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
