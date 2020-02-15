3ccdacbd1cf46804d7d77c991d0f5bf8f33cacd9
Steven Choi <stevencschoi87@gmail.com>
2020-02-15 03:58:09 +0000

    added conditional methods to solution

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
-let balance = 500.00;
-
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
   }
-}
+
+  get balance() {
+    // calculate the balance of transaction objects
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      // console.log(transaction);
+      // console.log("this is the value: ", transaction.value);
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+};
 
 class Transaction {
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
   }
-}
+
+  commit() {
+    if (!this.isAllowed()) return false;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+    return true;
+  }
+};
 
 class Deposit extends Transaction {
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
-}
+
+  isAllowed() {
+    return true;
+  }
+};
 
 class Withdrawal extends Transaction {
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
-
-}
+  // where is isAllowed defined??
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+};
 
 const myAccount = new Account("snow-patrol");
+console.log('Starting Balance', myAccount.balance);
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25, myAccount);
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+const t3 = new Deposit(120.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
